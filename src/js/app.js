e6b2cf279a0a39c88b965e3dc61c257a674716dd
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-10-31 17:02:34 +0100

    transition & legend

--- src/js/app.js
+++ src/js/app.js
@@ -7,30 +7,27 @@ const { data } = dataset
 const margin = {top: 20, right: 20, bottom: 30, left: 40}
 const width = 960 - margin.left - margin.right
 const height = 500 - margin.top - margin.bottom
-
-// set the ranges
 const x0 = d3.scaleBand().range([0, width]).padding(.25);
 const x1 = d3.scaleBand().padding(0);
 const y = d3.scaleLinear().range([height, 0]);
-      
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+
 const svg = d3.select("main").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", 
+const g = svg.append("g").attr("transform", 
       "translate(" + margin.left + "," + margin.top + ")");
-
-// Scale the range of the data in the domains
 
 x0.domain(data.map(d => d.name));
 x1.domain(data[0].vote.map(d => d.axis)).rangeRound([0, x0.bandwidth()]);
 y.domain([0, 5]);
-// y.domain([0, d3.max(data, (d) => d.vote[0].value)]);
 
-// append the rectangles for the bar chart
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x0))
+
+svg.append("g")
+  .call(d3.axisLeft(y).ticks(5));
+
 svg.append("g")
   .selectAll("g")
   .data(data)
@@ -39,18 +36,35 @@ svg.append("g")
   .selectAll("rect")
   .data(d => d.vote.map(d => {return {key: d.axis, value: d.value }}))
   .enter().append('rect')
-    .attr("x", d => x1(d.key))
-    .attr("y", d => y(d.value))
     .attr("width", x1.bandwidth())
-    .attr("height", d => height - y(d.value))
+    .attr("x", d => x1(d.key))
+    .attr("y", height)
+    .attr("height", 0)
     .attr("class", d => d.key.toLowerCase())
+    .transition()
+    .ease(d3.easeSinOut)
+    .delay((d, i) => i * 500 )
+    .duration(200)
+    .attr("y", d => y(d.value))
+    .attr("height", d => height - y(d.value))
 
 
-// add the x Axis
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x0))
+const legend = d3.select('main').append("svg")
+  .append("g")
+  .attr("text-anchor", "start")
+  .selectAll("g")
+  .data(data[0].vote.map(d => d.axis))
+  .enter().append("g")
+  .attr("transform", (d, i) => `translate(0, ${i * 20})`);
 
-// add the y Axis
-svg.append("g")
-  .call(d3.axisLeft(y));
+legend.append("rect")
+  .attr("x", 0)
+  .attr("width", 19)
+  .attr("height", 19)
+  .attr("class", d => d.toLowerCase());
+
+legend.append("text")
+  .attr("x",  24)
+  .attr("y", 9.5)
+  .attr("dy", "0.32em")
+  .text(d => d);
