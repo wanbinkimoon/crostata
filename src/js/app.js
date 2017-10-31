922ee7831a9ee06f78c65adda837f8e85032e56d
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-10-31 17:14:12 +0100

    interactinos

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,34 @@ const x0 = d3.scaleBand().range([0, width]).padding(.25);
 const x1 = d3.scaleBand().padding(0);
 const y = d3.scaleLinear().range([height, 0]);
 
+const legend = d3.select('main').append("svg")
+.append("g")
+.attr("text-anchor", "start")
+.selectAll("g")
+.data(data[0].vote.map(d => d.axis))
+.enter().append("g")
+.attr("transform", (d, i) => `translate(0, ${i * 20})`)
+.on('mouseenter', d => data[0].vote.filter(
+  t => t.axis !== d ? 
+  d3.selectAll(`.${t.axis.toLowerCase()}`).attr('opacity', '.25') :
+  d3.selectAll(`.${t.axis.toLowerCase()}`).attr('opacity', '1') 
+))
+.on('mouseleave', d =>data[0].vote.map(d => d3.selectAll(`.${d.axis.toLowerCase()}`).attr('opacity', '1')))
+
+
+legend.append("rect")
+.attr("x", 0)
+.attr("width", 19)
+.attr("height", 19)
+.attr("class", d => d.toLowerCase());
+
+legend.append("text")
+.attr("x",  24)
+.attr("y", 9.5)
+.attr("dy", "0.32em")
+.text(d => d);
+
+
 const svg = d3.select("main").append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
@@ -40,31 +68,13 @@ svg.append("g")
     .attr("x", d => x1(d.key))
     .attr("y", height)
     .attr("height", 0)
+    .attr("class", 'bar')
     .attr("class", d => d.key.toLowerCase())
     .transition()
     .ease(d3.easeSinOut)
-    .delay((d, i) => i * 500 )
-    .duration(200)
+    // .delay((d, i) => i * 200 )
+    .duration(350)
     .attr("y", d => y(d.value))
     .attr("height", d => height - y(d.value))
 
 
-const legend = d3.select('main').append("svg")
-  .append("g")
-  .attr("text-anchor", "start")
-  .selectAll("g")
-  .data(data[0].vote.map(d => d.axis))
-  .enter().append("g")
-  .attr("transform", (d, i) => `translate(0, ${i * 20})`);
-
-legend.append("rect")
-  .attr("x", 0)
-  .attr("width", 19)
-  .attr("height", 19)
-  .attr("class", d => d.toLowerCase());
-
-legend.append("text")
-  .attr("x",  24)
-  .attr("y", 9.5)
-  .attr("dy", "0.32em")
-  .text(d => d);
